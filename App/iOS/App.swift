36d1de6508c87cec330728671058515f23d35457
Naruki Chigira <tus.naru@gmail.com>
2023-07-08 23:23:00 +0900

    Add application delegate

--- App/iOS/App.swift
+++ App/iOS/App.swift
@@ -1,10 +1,22 @@
 import SwiftUI
+import UIKit
 
 @main
 struct FrameMintApp: App {
+    @UIApplicationDelegateAdaptor (AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().requestAuthorization(options:   [.alert, .sound, .badge]) {(granted, error) in
+            print("Permission granted: \(granted)")
+        }
+        return true
     }
 }
