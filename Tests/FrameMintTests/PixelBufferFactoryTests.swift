68feacea18e5d947caf5cefccd5af38baf0aed47
Naruki Chigira <tus.naru@gmail.com>
2023-07-09 13:45:21 +0900

    Add tests to convert pixel buffer

--- Tests/FrameMintTests/PixelBufferFactoryTests.swift
+++ Tests/FrameMintTests/PixelBufferFactoryTests.swift
@@ -146,4 +146,43 @@ final class PixelBufferFactoryTests: XCTestCase {
             XCTAssert(extraRowsOnBottom == 7)
         }
     }
+
+    @MainActor
+    func testConvertPixelBuffers() {
+        let canvasFactory = CanvasFactory()
+        let pixelBufferFactory = PixelBufferFactory.default
+
+        XCTContext.runActivity(named: "Convert 32ARGB to 420YpCbCr8BiPlanarFullRange 100x100") { _ in
+            let canvas = try! canvasFactory.makeCanvas(width: 100, height: 100, extent: .zero)
+            let pixelBuffer = try! pixelBufferFactory.make420YpCbCr8BiPlanarFullRangePixelBuffer(from: canvas)
+            XCTAssert(CVPixelBufferGetWidth(pixelBuffer) == 100)
+            XCTAssert(CVPixelBufferGetHeight(pixelBuffer) == 100)
+            XCTAssert(CVPixelBufferGetPixelFormatType(pixelBuffer) == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)
+        }
+
+        XCTContext.runActivity(named: "Convert 32ARGB to 420YpCbCr8BiPlanarFullRange 150x150") { _ in
+            let canvas = try! canvasFactory.makeCanvas(width: 150, height: 150, extent: .zero)
+            let pixelBuffer = try! pixelBufferFactory.make420YpCbCr8BiPlanarFullRangePixelBuffer(from: canvas)
+            XCTAssert(CVPixelBufferGetWidth(pixelBuffer) == 150)
+            XCTAssert(CVPixelBufferGetHeight(pixelBuffer) == 150)
+            XCTAssert(CVPixelBufferGetPixelFormatType(pixelBuffer) == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)
+        }
+
+        XCTContext.runActivity(named: "Convert 32ARGB to 420YpCbCr8BiPlanarFullRange 100x100 with extent") { _ in
+            let canvas = try! canvasFactory.makeCanvas(width: 100, height: 100, extent: .init(top: 5, left: 6, bottom: 7, right: 8))
+            let pixelBuffer = try! pixelBufferFactory.make420YpCbCr8BiPlanarFullRangePixelBuffer(from: canvas)
+            XCTAssert(CVPixelBufferGetWidth(pixelBuffer) == 100)
+            XCTAssert(CVPixelBufferGetHeight(pixelBuffer) == 100)
+            XCTAssert(CVPixelBufferGetPixelFormatType(pixelBuffer) == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)
+            var extraCloumnsOnLeft: Int = 0
+            var extraCloumnsOnRight: Int = 0
+            var extraRowsOnTop: Int = 0
+            var extraRowsOnBottom: Int = 0
+            CVPixelBufferGetExtendedPixels(pixelBuffer, &extraCloumnsOnLeft, &extraCloumnsOnRight, &extraRowsOnTop, &extraRowsOnBottom)
+            XCTAssert(extraCloumnsOnLeft == 6)
+            XCTAssert(extraCloumnsOnRight == 8)
+            XCTAssert(extraRowsOnTop == 5)
+            XCTAssert(extraRowsOnBottom == 7)
+        }
+    }
 }
